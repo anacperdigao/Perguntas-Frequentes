84845568bebad77ff9bb9756560b26073b55c931
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-09-27 10:45:11 -0300

    adicionando :root com as cores em variaveis

--- src/Components/PerguntasERespostas/index.js
+++ src/Components/PerguntasERespostas/index.js
@@ -2,13 +2,19 @@ import './PerguntasERespostas.css';
 import {ReactComponent as IconArrow} from '../../images/icon-arrow-down.svg';
 
 const PerguntasERespostas = ({pergunta, resposta}) => {
+    
+    const abreResposta = () => {
+        console.log('cliquei')
+    }
+    
+    
     return(
         <div className='perguntas-respostas'>
-            <div className='pergunta-imagem'>
+            <div onClick={abreResposta} className='pergunta-imagem'>
                 <p className='perguntas'>{pergunta}</p>
                 <IconArrow />
             </div>
-            <p className='respostas'>{resposta}</p>
+            <p className='respostas' hidden>{resposta}</p>
         </div>
     )
 }
