2b74c98df4b8a0290091e523ae3d50bbc3aa7a56
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-09-28 12:49:24 -0300

    feat: adicionando sobreposição de imagens

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import './App.css';
 import ContainerPerguntas from './Components/ContainerPerguntas';
 //import {ReactComponent as WomanOnline} from './images/illustration-woman-online-desktop.svg';
 import womanOnline from './images/illustration-woman-online-desktop.svg';
+import background from './images/bg-pattern-desktop.svg';
 
 
 function App() {
   return (
     <div className="App">
+        
         <div className='container__imagens'>
-          <img src={womanOnline} alt='Mulher no computador' className='imagem-principal' />
+          <img src={background} alt='Background com listras' className='imagem-background' />
+          <div className='imagem-sobreposicao'>  
+            <img src={womanOnline} alt='Mulher no computador' className='imagem-principal' />
+          </div>
         </div>
+        
         <ContainerPerguntas />
+        
     </div>
   );
 }
