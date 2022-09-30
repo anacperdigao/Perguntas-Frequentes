ff92055d12b44e89b6e558965d42da20a33a4c3b
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-09-30 11:23:21 -0300

    Finalização do projeto com responsividade

--- src/Components/ContainerGeral/style.js
+++ src/Components/ContainerGeral/style.js
@@ -6,12 +6,16 @@ export const PaginaPrincipal = styled.div`
   padding: 0px 0px;
   background: linear-gradient(to bottom, ${softViolet}, ${softBlue});
   background-repeat: no-repeat;
+  background-size: cover;
   height: 100vh;
   width: 100vw;
   display: flex;
   align-items: center;
   justify-content: center;
-  box-sizing: border-box;  
+  
+  @media screen and (max-width: 480px){
+    height: 1200px;
+  }
 `;
 
 
@@ -27,10 +31,13 @@ export const ContainerPrincipal = styled.div`
   padding: 20px 10px;
   
   @media screen and (max-width: 480px){
-    grid-template-rows: 50% 50%;
-    grid-template-columns: none;
+    display: flex;
+    flex-direction: column;
     width: 270px;
-    padding: 0px 10px 60px 10px;
+    padding: 10px 10px 60px 10px;
+    position: absolute;
+    top: 50px;
+    box-sizing: border-box;
   }
 `;
 
